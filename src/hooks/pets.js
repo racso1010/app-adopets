50db4dc997650648e3ab0c5632f7ad28c18d4b8e
Oscar Lopez <oscalv93@gmail.com>
2023-10-29 07:21:59 -0400

    Base structure for single pet view

--- src/hooks/pets.js
+++ src/hooks/pets.js
@@ -1,8 +1,10 @@
 import axios from '@/lib/axios'
 
-export const getPets = async () => {
+export const getPets = async (id = null) => {
+    const url = `/api/pets/${id == null ? '' : id}`
+    console.log(url)
     return await axios
-        .get('/api/pets')
+        .get(url)
         .then(e => {
             return e.data
         })
