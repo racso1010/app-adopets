1714edaffe93d1d0bff8b4706be7b034812193a5
Oscar Lopez <oscalv93@gmail.com>
2023-10-29 14:18:04 -0400

    Profile

--- src/components/PetImage.js
+++ src/components/PetImage.js
@@ -1,13 +1,8 @@
-import { useState, useRef } from 'react'
-import Image from 'next/image'
-
 function PetImage({ image }) {
-    const ref = useRef()
-
     return (
         <div className="w-full md:w-1/2 max-w-md border border-palette-lighter bg-white rounded shadow-lg">
             <div className="relative h-96">
-                <Image
+                <img
                     src={image}
                     alt="pet-image"
                     layout="fill"
--- src/components/PetSection.js
+++ src/components/PetSection.js
@@ -2,12 +2,12 @@ import PetImage from '@/components/PetImage'
 import PetDetails from '@/components/PetDetails'
 
 function PetSection({ petData }) {
-  return (
-    <div className="flex flex-col justify-center items-center md:flex-row md:items-start space-y-8 md:space-y-0 md:space-x-4 lg:space-x-8 max-w-6xl w-11/12 mx-auto">
-      <PetImage images={petData.image} />
-      <PetDetails petData={petData} />
-    </div>
-  )
+    return (
+        <div className="flex flex-col md:flex-row md:items-start md:space-y-0 pt-8 mx-auto">
+            <PetImage image={petData.image} />
+            <PetDetails petData={petData} />
+        </div>
+    )
 }
 
 export default PetSection
